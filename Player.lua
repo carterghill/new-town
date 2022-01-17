607a86bce4f8a084292cd294c0b80a6b082762f1
Carter Hill <carter@pop-os.localdomain>
2022-01-16 23:28:13 -0600

    added accel, friction, and top speed to player movement

--- Player.lua
+++ Player.lua
@@ -8,12 +8,15 @@ Player = {
     dy = 0;
     width = 64;
     height = 64;
+    accel = 1000;
+    friction = 1000;
+    topSpeed = 250;
     controls = Controls:new(),
     input = Input:new(Keyboard:new())
 }
 
 function Player:new(x, y)
-    local s = setmetatable( {Player}, { __index = self } )
+    local s = setmetatable( { Player }, { __index = self } )
     s.x = x or 0
     s.y = y or 0
     s.controls = Controls:new()
@@ -23,43 +26,54 @@ end
 function Player:update(dt)
     
     if self.controls.up then
-        self.dy = self.dy + 1
+        self.dy = self.dy + self.accel*dt
     elseif self.dy > 0 then
-        if self.dy < 1 then
+        if self.dy < self.friction*dt then
             self.dy = 0
         else 
-            self.dy = self.dy - 1
+            self.dy = self.dy - self.friction*dt
         end
     end
 
     if self.controls.down then
-        self.dy = self.dy - 1
+        self.dy = self.dy - self.accel*dt
     elseif self.dy < 0 then
-        if self.dy > -1 then
+        if self.dy > -self.friction*dt then
             self.dy = 0
         else 
-            self.dy = self.dy + 1
+            self.dy = self.dy + self.friction*dt
         end
     end
 
     if self.controls.left then
-        self.dx = self.dx - 1
+        self.dx = self.dx - self.accel*dt
     elseif self.dx < 0 then
-        if self.dx > -1 then
+        if self.dx > -self.friction*dt then
             self.dx = 0
         else 
-            self.dx = self.dx + 1
+            self.dx = self.dx + self.friction*dt
         end
     end
 
     if self.controls.right then
-        self.dx = self.dx + 1
+        self.dx = self.dx + self.accel*dt
     elseif self.dx > 0 then
-        if self.dx < 1 then
+        if self.dx < self.friction*dt then
             self.dx = 0
         else 
-            self.dx = self.dx - 1
+            self.dx = self.dx - self.friction*dt
         end
+    end
+
+    if self.dx > self.topSpeed then
+        self.dx = self.topSpeed
+    elseif self.dx < -self.topSpeed then
+        self.dx = -self.topSpeed
+    end
+    if self.dy > self.topSpeed then
+        self.dy = self.topSpeed
+    elseif self.dy < -self.topSpeed then
+        self.dy = -self.topSpeed
     end
 
     self.x = self.x + self.dx*dt
