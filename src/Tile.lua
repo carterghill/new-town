23d082d4e853aec35b910303b98aa5f47d978688
carteradaptive <Carter@adaptiveagriculture.ca>
2022-01-21 00:20:32 -0600

    Tile draws by its defined x and y

--- src/Tile.lua
+++ src/Tile.lua
@@ -1,14 +1,24 @@
 require('src/Camera')
 
-Tile = {}
+Tile = {
+    img = nil,
+    sx = 1,
+    sy = 1,
+    x = 0,
+    y = 0
+}
 
-function Tile:new(img)
+function Tile:new(img, x, y, size)
     local t = setmetatable( { Tile }, { __index = self } )
-    t.img = love.graphics.newImage(img)
-    t.scale = 64/t.img:getWidth()
+    local s = size or 64
+    t.img = img
+    t.sx = s/t.img:getWidth()
+    t.sy = s/t.img:getHeight()
+    t.x = x
+    t.y = y
     return t
 end
 
-function Tile:draw(x, y)
-    love.graphics.draw(self.img, x - Camera.x, y - Camera.y, 0, self.scale)
+function Tile:draw()
+    love.graphics.draw(self.img, self.x - Camera.x, self.y - Camera.y, 0, self.sx, self.sy)
 end
--- src/Tiles.lua
+++ src/Tiles.lua
@@ -6,32 +6,18 @@ Tiles = {
 }
 
 function Tiles:load()
-    Dirt = Tile:new('assets/Tilesets/Dirt/Dirt_01.png')
-    local files = love.filesystem.getDirectoryItems('assets/Tilesets/Dirt')
-    for i, file in ipairs(files) do
-        self.dirt[#self.dirt] = love.graphics.newImage('assets/Tilesets/Dirt/'..file)
-    end
-    files = love.filesystem.getDirectoryItems('assets/Tilesets/Tilled')
-    for i, file in ipairs(files) do
-        self.tilled[#self.tilled] = love.graphics.newImage('assets/Tilesets/Tilled/'..file)
+    local dirtimg = love.graphics.newImage('assets/Tilesets/Dirt/Dirt_01.png')
+    Dirt = Tile:new(dirtimg)
+    for i = -25, 25, 1 do
+        for j = -25, 25, 1 do
+            print(#self.dirt)
+            self.dirt[#self.dirt+1] = Tile:new(dirtimg, i*256, j*256, 256)
+        end
     end
 end
 
 function Tiles:draw()
-    for i = 1, 10, 1 do
-        for j = 1, 10, 1 do
-            Dirt:draw(64*(i), 64*j)
-        end
+    for key, value in pairs(self.dirt) do
+        value:draw()
     end
-    --[[love.graphics.draw(self.dirt[0], 0, 0, 0, 0.25)
-    love.graphics.draw(self.dirt[0], 0, 128, 0, 0.25)
-    love.graphics.draw(self.dirt[0], 0, 256, 0, 0.25)
-    love.graphics.draw(self.dirt[0], 128, 0, 0, 0.25)
-    love.graphics.draw(self.dirt[0], 256, 0, 0, 0.25)
-    love.graphics.draw(self.dirt[0], 256+128, 0, 0, 0.25)
-
-    love.graphics.draw(self.tilled[0], 128, 128, 0, 0.25)
-    love.graphics.draw(self.tilled[0], 256, 128, 0, 0.25)
-    love.graphics.draw(self.tilled[0], 128, 256, 0, 0.25)
-    love.graphics.draw(self.tilled[0], 256, 256, 0, 0.25)]]--
 end
