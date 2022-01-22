63cff6d0db0e172d4bc281a4d98e35643b456ac7
Carter Hill <carterghill@gmail.com>
2022-01-22 11:53:30 -0600

    Added zoom to the tiles and got rid of tile display width and height

--- src/Tileset.lua
+++ src/Tileset.lua
@@ -18,20 +18,18 @@ function Tileset:new(image)
 
     mapX = 1
     mapY = 1
-    tilesDisplayWidth = 26
-    tilesDisplayHeight = 20
     
     zoomX = 2
     zoomY = 2
 
     tilesetImage = love.graphics.newImage( image ) 
     -- this "linear filter" removes some artifacts if we were to scale the tiles
-    --tilesetImage:setFilter("nearest", "linear") 
+    tilesetImage:setFilter("nearest", "linear") 
     tileSize = 32
     
     -- grass
     tileQuads = {}
-    tileQuads[0] = love.graphics.newQuad(0 * tileSize, 20 * tileSize, tileSize, tileSize,
+    tileQuads[0] = love.graphics.newQuad(0 * tileSize, 0 * tileSize, tileSize, tileSize,
         tilesetImage:getWidth(), tilesetImage:getHeight())
     -- kitchen floor tile
     tileQuads[1] = love.graphics.newQuad(2 * tileSize, 0 * tileSize, tileSize, tileSize,
@@ -43,20 +41,20 @@ function Tileset:new(image)
     tileQuads[3] = love.graphics.newQuad(3 * tileSize, 9 * tileSize, tileSize, tileSize,
         tilesetImage:getWidth(), tilesetImage:getHeight())
 
-    t.batch = love.graphics.newSpriteBatch(tilesetImage, tilesDisplayWidth * tilesDisplayHeight)
+    t.batch = love.graphics.newSpriteBatch(tilesetImage, mapWidth * mapHeight)
 
     return t
 end 
 
 function Tileset:draw()
-    love.graphics.draw(self.batch, math.floor(-zoomX*(mapX%1)*tileSize - Camera.x), math.floor(-zoomY*(mapY%1)*tileSize - Camera.y),
+    love.graphics.draw(self.batch, math.floor(-Camera.x), math.floor(-Camera.y),
     0, zoomX, zoomY)
 end
 
 function Tileset:update()
     self.batch:clear()
-    for x=0, tilesDisplayWidth-1 do
-      for y=0, tilesDisplayHeight-1 do
+    for x=0, mapWidth-1 do
+      for y=0, mapHeight-1 do
         self.batch:add(tileQuads[map[x+mapX][y+mapY]], x*tileSize, y*tileSize)
       end
     end
