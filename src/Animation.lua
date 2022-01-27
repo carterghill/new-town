4ce65d0a9e79ea83fdd986803e1bf3fc98b20c0c
Carter Hill <carterghill@gmail.com>
2022-01-26 23:27:04 -0600

    All frames of waterfall now properly render in sequense at the given frame rate

--- src/Animation.lua
+++ src/Animation.lua
@@ -7,17 +7,20 @@ Animation = {
     zy = 1,
     width = 0,
     height = 0,
-    tileWidth = 64,
-    tileHeight = 64
+    fps = 12,
+    frameAge = 0,
+    frame = 1
 }
 
-function Animation:new(img, width, height, zx, zy)
+function Animation:new(img, fps, width, height, zx, zy)
+
     local a = setmetatable( { Animation }, { __index = self } )
 
     a.image = img
     a.image:setFilter("nearest") 
     a.zx = sx or 1
     a.zy = sy or 1
+    a.fps = fps or 12
 
     local imageWidth = a.image:getWidth()
     local imageHeight = a.image:getHeight()
@@ -26,80 +29,34 @@ function Animation:new(img, width, height, zx, zy)
     local quads = {}
     local tilesx = imageWidth/tileWidth
     local tilesy = imageHeight/tileHeight
+
     for y = 1, tilesy, 1 do
         for x = 1, tilesx, 1 do
-            quads[x + (y-1)*tilesy] = love.graphics.newQuad(x-1, y-1, tileWidth, tileHeight, imageWidth, imageHeight)
+            quads[x + (y-1)*tilesx] = love.graphics.newQuad((x-1)*tileWidth, (y-1)*tileHeight, tileWidth, tileHeight, imageWidth, imageHeight)
+            print(x.." + ("..y.."-1)*"..tilesx.." = "..(x + (y-1)*tilesx))
         end
     end
 
     a.quads = quads
     a.batch = love.graphics.newSpriteBatch(a.image, tilesx * tilesy)
 
-    --[[for i = 1, (imageWidth*imageHeight)/128, 128 do
-        quads[math.floor(i/128)+1] = love.graphics.newQuad(math.floor((i-1)%imageWidth), 
-            math.floor((i-1)/imageWidth)*tileSize, tileSize, tileSize,
-            a.image:getWidth(), a.image:getHeight())
-    end
-
-    local w = t.file.width
-    local h = t.file.height
-    t.mapWidth = w
-    t.mapHeight = h
-    t.tileWidth = t.file.tilewidth
-    t.tileHeight = t.file.tileheight
-    t.zx = 64/tileSize
-    t.zy = 64/tileSize
-    t.width = w * t.tileWidth * t.zx
-    t.height = h * t.tileHeight * t.zy
-
-    t.batch = love.graphics.newSpriteBatch(t.images[1], t.mapWidth * t.mapHeight)
-    t.batchLower = love.graphics.newSpriteBatch(t.images[1], t.mapWidth * t.mapHeight)
-    t.batchHigher = love.graphics.newSpriteBatch(t.images[1], t.mapWidth * t.mapHeight)
-
-    t.quads = quads
-    t.imageWidth = imageWidth
-    t.imageHeight = imageHeight
-    t.layers = {}
-
-    for index, layer in ipairs(t.file.layers) do
-        local map = {}
-        if layer.data ~= nil then
-            for i, v in ipairs(layer.data) do
-                if map[(i-1)%w + 1] == nil then
-                    map[(i-1)%w + 1] = {}
-                end
-                map[(i-1)%w + 1][math.floor((i-1)/(w))+1] = v
-            end
-        end
-        if layer.name == "Collision" then
-            t.collision = map
-        elseif layer.type == "objectgroup" then
-            t.objects = layer.objects
-        else
-            t.layers[#t.layers+1] = map
-        end
-    end
-
-    local map = {}
-    for i, v in ipairs(t.file.layers[1].data) do
-        if map[(i-1)%w + 1] == nil then
-            map[(i-1)%w + 1] = {}
-        end
-        map[(i-1)%w + 1][math.floor((i-1)/(w))+1] = v
-    end
-    t.map = map--]]
-
     return a
 end
 
-function Animation:update()
-    local mapX = 1
-    local mapY = 1
-    self.batch:clear()
-
-    self.batch:add(self.quads[1], 0, 0)
-
-    self.batch:flush()
+function Animation:update(dt)   
+    local frameTime = 1/self.fps
+    self.frameAge = self.frameAge + dt
+    if self.frameAge > frameTime then
+        if self.frame == #self.quads then
+            self.frame = 1
+        else 
+            self.frame = self.frame + 1
+        end
+        self.frameAge = 0
+        self.batch:clear()
+        self.batch:add(self.quads[self.frame])
+        self.batch:flush()
+    end
 end
 
 function Animation:draw(x, y)
